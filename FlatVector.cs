cfa1034b2009184352040ce1ef121e4d242ea103
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-06-03 22:22:52 -0700

    desc

--- FlatVector.cs
+++ FlatVector.cs
@@ -1,4 +1,6 @@
 /* 
+ * Lightweight, high-performing vector class.
+ *
  * Building block of physics engine,
- * Representing all objects on a 2-dimensional grid
+ * Representing all objects on a 2-dimensional grid.
 */
