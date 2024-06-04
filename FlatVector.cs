af04de4d498d6f144c948a153e2ecdaef4e76086
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-06-03 22:24:39 -0700

    initialize

--- FlatVector.cs
+++ FlatVector.cs
@@ -4,3 +4,17 @@
  * Building block of physics engine,
  * Representing all objects on a 2-dimensional grid.
 */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlatPhysics
+{
+  public readonly struct FlatVector
+  {
+    
+  }
+}
