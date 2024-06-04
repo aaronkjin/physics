bb685797f20f4752fa3f6434ec36281bac224fa6
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-06-03 22:33:12 -0700

    initialize sim

---
+++ Simulation.cs
@@ -0,0 +1,20 @@
+/*
+ * MonoGame to simulate games for the physics engine.
+ *
+ * Logic for key inputs, movements, camera, and more.
+*/
+
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Flat;
+using Flat.Graphics;
+using Flat.Input;
+
+namespace FlatPhysics
+{
+  
+}
