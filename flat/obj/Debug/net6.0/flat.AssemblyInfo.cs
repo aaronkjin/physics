6c4b3ecc82010fb8bee520b616fbfb1f9e1880ff
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-07-08 20:06:29 -0700

    more cache, but it's a success!

--- flat/obj/Debug/net6.0/flat.AssemblyInfo.cs
+++ flat/obj/Debug/net6.0/flat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("flat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2831b2785c5eb3cf01309eb624d4b4013f3e4fbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd098ef0fe9bdb3a4524213e42fbd2def8e7a014")]
 [assembly: System.Reflection.AssemblyProductAttribute("flat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("flat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
