a3d52cc9e47225bb956c4437ad950f68d51cf132
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-07-30 21:19:38 -0700

    convert to monogame vector

--- tests/FlatConverter.cs
+++ tests/FlatConverter.cs
@@ -1,6 +1,20 @@
 /*
  * Converts FlatVectors to MonoGame
- * vectors.
+ * vectors, and vice versa.
 */
 
-using 
+using System;
+using Microsoft.Xna.Framework;
+
+using engine;
+
+namespace tests
+{
+  public class FlatConverter
+  {
+    public static Vector2 ToVector2(FlatVector v)
+    {
+      return new Vector2(v.X, v.Y);
+    }
+  }
+}
