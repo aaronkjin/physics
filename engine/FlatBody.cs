b1af3b079120ec05adf22e91211b6b60f4f6c66a
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-08-26 23:49:50 -0700

    body properties

--- engine/FlatBody.cs
+++ engine/FlatBody.cs
@@ -4,6 +4,16 @@ namespace engine
 {
   public sealed class FlatBody
   {
+    private FlatVector position;
+    private FlatVector linearVelocity;
+    private float rotation;
+    private float rotationalVelocity;
 
+    private float density;
+    private float mass;
+    private float restitution; // Bounce!
+    private float area;
+    
+    private bool isStatic;
   }
 }
