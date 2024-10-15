509ca0c8b018769905a8b70b88e2d5d5b54c962f
aaronkjin <aaronkjin@gmail.com>
2024-10-14 17:57:24 -0700

    init constants for our world

--- engine/FlatWorld.cs
+++ engine/FlatWorld.cs
@@ -4,6 +4,12 @@ namespace engine
 {
   public sealed class FlatWorld
   {
+    // cm
+    public static readonly float MinBodySize = 0.01f * 0.01f;
+    public static readonly float MaxBodySize = 64f * 64f;
 
+    // g / cm^3
+    public static readonly float MinDensity = 0.5f;
+    public static readonly float MaxDensity = 21.4f;
   }
 }
